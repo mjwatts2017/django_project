16e7a1dc352fc85566f6e841654409ee3ac5a39d
Mjwatts2017 <mjwatts2017@gmail.com>
2025-02-09 14:30:29 -0500

    Created a view that changes the colour of a grid. Next step is to have control over each grid with a click of a button.

--- django_project/path_visualization_app/views.py
+++ django_project/path_visualization_app/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+import random
+
+# def home(request):
+#     return render(request, "home.html")
 
 def home(request):
-    return render(request, "home.html")
+    grid_size = 20
+    grid_data = [[0] * grid_size for _ in range(grid_size)]  # Initialize grid with zeros
+    context = {'grid': grid_data, 'grid_size': grid_size}
+    return render(request, 'home.html', context)
